bb25e87b88ba591b7893bc517bee82efb934f6fc
Fanny <fannylundgreen93@gmail.com>
2018-11-28 19:37:07 +0100

    last css

--- js/Purchasepage.js
+++ js/Purchasepage.js
@@ -141,7 +141,7 @@ document.getElementById("Checkout-btn").addEventListener("click", function() {
             activeUser.shoppingCart = []
 
             localStorage.setItem('users',JSON.stringify(users));
-            // 3. empty shopping cart when order is created
+// 3. Empty shopping cart when order is created
 
 
               window.open("checkoutcompleted.html");
